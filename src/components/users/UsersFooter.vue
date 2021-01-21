da45644127340e444d9cb06a6b8b6db0c70ac15a
= <=>
2021-01-21 14:47:09 +0200

    Named routes

---
+++ src/components/users/UsersFooter.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>This is User footer</h1>
+</template>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import NotFound from './components/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -15,24 +17,42 @@ const router = createRouter({
             redirect: '/teams'
         },
         {
+            name: 'teams',
             path: '/teams',
-            component: TeamsList,
+            components: {
+                default: TeamsList,
+                footer: TeamsFooter
+            },
+            children: [
+                {
+                    name: 'team-members',
+                    path: ':teamId',
+                    component: TeamMembers,
+                    props: true
+                },
+            ]
         },
         {
             path: '/users',
-            component: UsersList
+            components: {
+                default: UsersList,
+                footer: UsersFooter
+            }
         },
-        {
-            path: '/teams/:teamId',
-            component: TeamMembers,
-            props: true
-        },
+       
         {
             path: '/:notFound(.*)',
             component: NotFound
         }
     ],
-    linkActiveClass: 'active-link'
+    linkActiveClass: 'active-link',
+    scrollBehavior(to, from, savedPosition){
+        console.log(to, from, savedPosition);
+        if(savedPosition){
+            return savedPosition;
+        }
+        return {left: 0, top:0}
+    }
 });
 
 const app = createApp(App)
